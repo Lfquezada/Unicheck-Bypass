e880253ad657acb27c77b5160f3922ea9e2173c7
lquezada <que18028@uvg.edu.gt>
2019-05-10 00:24:27 -0600

    Random unique code generator added
    Missing: image manipulation

--- src/Bypasser.java
+++ src/Bypasser.java
@@ -1,7 +1,12 @@
 
 import java.io.IOException;
+import java.awt.Color;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import java.util.ArrayList;
+
 
 public class Bypasser {
 
@@ -9,12 +14,51 @@ public class Bypasser {
 	public static void main(String args[]) {
 
 
-		String fileName = "EmptyPDF.pdf";
+		String fileName = "newPDF.pdf";
+		WordsGenerator wordsGenerator = new WordsGenerator();
+		String line;
 
 		try {
 
 			PDDocument doc = new PDDocument();
-			doc.addPage(new PDPage());
+			PDPage page = new PDPage();
+			doc.addPage(page);
+
+			PDPageContentStream content = new PDPageContentStream(doc, page);
+
+			content.beginText();
+			content.setFont(PDType1Font.TIMES_ROMAN, 12);
+			content.setNonStrokingColor(Color.RED);
+			content.newLineAtOffset(50, 60);
+			line = wordsGenerator.getWords(10);
+			content.showText(line);
+			content.endText();
+			
+			content.beginText();
+			content.setFont(PDType1Font.TIMES_ROMAN, 12);
+			content.setNonStrokingColor(Color.RED);
+			content.newLineAtOffset(50, 50);
+			line = wordsGenerator.getWords(10);
+			content.showText(line);
+			content.endText();
+
+			content.beginText();
+			content.setFont(PDType1Font.TIMES_ROMAN, 12);
+			content.setNonStrokingColor(Color.RED);
+			content.newLineAtOffset(50, 40);
+			line = wordsGenerator.getWords(10);
+			content.showText(line);
+			content.endText();
+
+			content.beginText();
+			content.setFont(PDType1Font.TIMES_ROMAN, 12);
+			content.setNonStrokingColor(Color.RED);
+			content.newLineAtOffset(50, 30);
+			line = wordsGenerator.getWords(10);
+			content.showText(line);
+			content.endText();
+
+			content.close();
 
 			doc.save(fileName);
 			doc.close();
---
+++ src/WordsGenerator.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.lang.Math;
+
+
+public class WordsGenerator {
+
+	String chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890,.;:()[]{}!?<>@#$%^&*";
+	String words;
+	Random random = new Random();
+	static final int WORDLENGTH = 5;
+	int charIndex;
+
+	public WordsGenerator() {
+
+	}
+
+	public String getWords(int numWords) {
+		
+		words = "";
+
+		for (int i=0; i<numWords; i++) {
+
+			// formar una palabra
+			for (int x=0; x<WORDLENGTH; x++) {
+
+				charIndex = random.nextInt(chars.length()-1); // asegurar que no sea 0
+				charIndex++; // charIndex puede tener de 1 a el length de char
+
+				words += (chars.subSequence(charIndex, (charIndex+1)));
+			}
+
+			words += " "; // fin de una palabra
+		}
+		// fin de la linea
+		return words;
+	}
+
+}
